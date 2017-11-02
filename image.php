0156232e4050a28899cba729a2bf79d2afa159d8
Yonas Robi <yonas.robi@gmail.com>
2017-11-02 14:40:13 -0400

    updating color again

--- image.php
+++ image.php
@@ -10,7 +10,7 @@ if ( $deployment == 'blue') {
 } elseif ($deployment == 'green')  {
   $color = imagecolorallocate($im, 0, 255, 0);
 } else {
-  $color = imagecolorallocate($im, 0, 0, 255);
+  $color = imagecolorallocate($im, 0, 0, 0);
 }
 
 // Draw a filled rectangle
